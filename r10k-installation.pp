e1776409eb987effd8bb573b6d3bbd8bb05ac926
Ram Maram <ramtillu87@gmail.com>
2016-06-12 19:47:03 -0400

    updating r10k

--- r10k-installation.pp
+++ r10k-installation.pp
@@ -1,14 +1,14 @@
-include r10k::mcollective
+/*include r10k::mcollective
 
 class {'r10k::webhook::config':
-  enable_ssl => true,
-  protected  => true,
-  notify     => Service['webhook'],
+  enable_ssl => false,
+  protected  => false,
 }
 
 class {'r10k::webhook':
   require => Class['r10k::webhook::config'],
 }
+
 
 # https://github.com/abrader/abrader-gms
 # Add webhook to control repository ( the repo where the Puppetfile lives )
@@ -16,9 +16,20 @@ class {'r10k::webhook':
 git_webhook { 'web_post_receive_webhook' :
   ensure             => present,
   webhook_url        => 'https://puppet:puppet@master:8088/payload',
-  token              =>  hiera('github_api_token'),
+  token              => 'xxxx',
   project_name       => 'goutham27/puppet-control',
   server_url         => 'https://api.github.com',
   disable_ssl_verify => true,
   provider           => 'github',
+}
+*/
+class { 'r10k':
+  configfile => '/etc/puppetlabs/r10k/r10k.yaml',
+  sources    => {
+    'puppet'    => {
+      'remote'  => 'https://github.com/goutham27/puppet-control.git',
+      'basedir' => "${::settings::environmentpath}",
+      'prefix'  => false,
+    }
+  },
 }
