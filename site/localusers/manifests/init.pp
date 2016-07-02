30798e646ee4e3070ed37fa07d7f1f80415a7ba2
Ram Maram <ramtillu87@gmail.com>
2016-07-02 19:49:26 -0400

    adding localusers to hiera

--- site/localusers/manifests/init.pp
+++ site/localusers/manifests/init.pp
@@ -1,11 +1,12 @@
 class localusers {
+	
   user { 'admin':
     ensure     => present,
     shell      => '/bin/bash',
     home       => '/home/admin',
     gid        => 'wheel',
     managehome => true,
-    password   => 'encrypted password', 
+    password   => 'password123', 
   }
 
   user {'jeff' :
