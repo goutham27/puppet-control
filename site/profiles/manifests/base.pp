8c6d4def38825ee15484168b804d7e49715203bb
Ram Maram <ramtillu87@gmail.com>
2016-07-03 20:13:53 -0400

    adding apache module

--- site/profiles/manifests/base.pp
+++ site/profiles/manifests/base.pp
@@ -2,7 +2,7 @@ class profiles::base {
   
   include base
   include base::params
-  include base:ssh
+  include base::ssh
   include base::variables
   include localusers
   include localusers::groups::finance
