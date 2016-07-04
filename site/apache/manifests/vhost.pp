86690b5018d4d5eca3f3b9d279c70c51d592559e
Ram Maram <ramtillu87@gmail.com>
2016-07-03 22:57:48 -0400

    fixing lint issues

--- site/apache/manifests/vhost.pp
+++ site/apache/manifests/vhost.pp
@@ -14,7 +14,7 @@ define apache::vhost ($port, $document_root, $server_name, $vhost_name = '*', $v
   file {'config_file':
     path    => "${vhost_dir}/${servername}.conf",
     content => template('apache/vhost.conf.erb'),
-    reqire  => Package['apache'],
+    require  => Package['apache'],
     notify  => Service['apache']
   }
 }
